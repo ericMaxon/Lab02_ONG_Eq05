a95bef600303a652dccbb682da3f30827b6be751
EricMaxon <57864479+ericMaxon@users.noreply.github.com>
2021-06-06 12:22:39 -0500

    Interfaces

---
+++ Clases/Interfaces/INav.vb
@@ -0,0 +1,4 @@
+﻿Public Interface INav
+    Sub NavegarA(ByVal ventanaActual As Form, ByVal nuevaVentana As Form)
+    Sub Regresar(ByVal ventanaActual As Form)
+End Interface
---
+++ Clases/Interfaces/IProducto.vb
@@ -0,0 +1,7 @@
+﻿Public Interface IProducto
+    Sub SetDest(ByVal dest As String)
+    Sub SetNombre(ByVal name As String)
+    Function GetDest() As String
+    Function GetNombre() As String
+    Function Inventario() As Integer
+End Interface
