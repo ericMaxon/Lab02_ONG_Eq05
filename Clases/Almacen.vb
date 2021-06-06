17dd538144617824747402c5bd8dbef5b4bffc2b
EricMaxon <57864479+ericMaxon@users.noreply.github.com>
2021-06-06 12:22:51 -0500

    Clases principales

---
+++ Clases/Almacen.vb
@@ -0,0 +1,19 @@
+﻿Public Class Almacen
+    Private inventario As New List(Of Vacunas)
+    Private ubicacion As String
+
+    Public Sub New(ubicacion As String)
+        Me.ubicacion = ubicacion
+    End Sub
+
+    Public Sub InsertarProductos(secuencia As String, cant As Integer)
+        Dim producto =
+        If() Then
+            inventario.Add(New Vacunas(secuencia, cant))
+        End If
+    End Sub
+
+    Public Function NivelDeInventario()
+
+    End Function
+End Class
